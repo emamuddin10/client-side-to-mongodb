f31e8452d8bbf614f4cfeba1b0326dd34501989c
Emam Uddin <emamskingdom@gmail.com>
2025-03-11 22:14:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+  
+const handleAddUser = e =>{
+  e.preventDefault()
+  console.log(e.target)
+  const form= e.target
+  const name = form.name.value
+  const email = form.email.value 
+  const user= {name,email}
+  console.log(user)
+  fetch('http://localhost:5000/users',{
+    method: "POST",
+    headers:{
+      'content-type':"application/json"
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res=> res.json())
+  .then(data => console.log(data))
+}
+  return (
+    <>
+      <h1>Simple Crud Server</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <input type="email" name="email" id="" />
+        <input type="submit" value="Add User" />
+      </form>
+      
+    </>
+  )
+}
+
+export default App
