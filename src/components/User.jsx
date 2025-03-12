0609bd233d66f3f53a7de12b0e8954c0c3c8d2b0
Emam Uddin <emamskingdom@gmail.com>
2025-03-13 02:32:16 +0600

    added map data

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const User = () => {
+    const users = useLoaderData()
     return (
         <div>
-            
+            {users.length}
+            <div>
+                {
+                    users.map(user => <p> {user.name} : {user.email}</p>)
+                }
+            </div>
         </div>
+        
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import User from './components/User.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
   },
+  {
+    path:'/users',
+    element:<User></User>,
+    loader:()=> fetch('http://localhost:5000/users')
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
