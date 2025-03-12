e01ad2a46047cdad083c57dac26e40f01385ea40
Emam Uddin <emamskingdom@gmail.com>
2025-03-13 01:43:35 +0600

    adden app

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,13 @@ const handleAddUser = e =>{
     body: JSON.stringify(user)
   })
   .then(res=> res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data)
+    if(data.insertedId){
+      alert('Users Added successfully')
+      form.reset()
+    }
+  })
 }
   return (
     <>
